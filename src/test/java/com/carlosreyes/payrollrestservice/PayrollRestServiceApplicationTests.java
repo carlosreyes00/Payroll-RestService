05aed1412b20db5b6a13133985b76b28331b70a6
Carlos Reyes <82222006+carlosreyes00@users.noreply.github.com>
2025-12-14 11:58:36 -0500

    Initial Commit

---
+++ src/test/java/com/carlosreyes/payrollrestservice/PayrollRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosreyes.payrollrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollRestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
